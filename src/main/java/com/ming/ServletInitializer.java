34e2a9343b97f20cd99c6bbecd842333278128d8
mingo2017 <minie1234@nate.com>
2017-02-07 13:56:19 +0900

    first commit

---
+++ src/main/java/com/ming/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ming;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BoardApplication.class);
+	}
+
+}
